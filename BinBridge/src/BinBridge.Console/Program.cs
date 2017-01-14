564cdc9354bc0b7e7b6e63cbc8e9b7dcb8b641b4
mazong1123 <mazong1123@gmail.com>
2017-01-14 19:22:33 +0800

    Implemented remote command exuecution.

--- BinBridge/src/BinBridge.Console/Program.cs
+++ BinBridge/src/BinBridge.Console/Program.cs
@@ -10,6 +10,21 @@ namespace BinBridge.Console
     {
         public static void Main(string[] args)
         {
+            string host = "127.0.1.1";
+            string userName = "mazong1123";
+            string password = "mazong1123!@#$";
+
+            string srcDirectory = "";
+            string destDirectory = "/home/mazong1123/binbridge/test";
+
+            using (SshClient sshClient = new SshClient(host, userName, password))
+            {
+                sshClient.Connect();
+                var command = sshClient.CreateCommand("find " + destDirectory + " -type f -exec ls -l --time-style=full-iso {} \\;");
+                var remoteResult = command.Execute();
+
+
+            }
         }
     }
 }
